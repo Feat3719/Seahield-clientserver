3e020f3a11a54d1a4eaee38e59a0e75398149059
quack <rudals23yoo@naver.com>
2024-01-30 15:41:36 +0900

    경민_수정중

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import './App.css';
+import Homepage from './Homepage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Sidenav from './sidenav/Sidenav';
+
 
 function App() {
+
+  const main = <Sidenav/>;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          <p>asdf</p>
-          <p>asdf</p>
-        </a>
-      </header>
+    <div>
+      <Sidenav/>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Homepage />} />
+          
+        </Routes>
+
+      </BrowserRouter>
     </div>
+
+    
   );
 }
 
---
+++ src/sidenav/Sidenav.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import style from './Sidenav.module.css';
+
+
+function Sidenav() {
+
+    return (
+        <div >
+            <h1 className={style.homepage_nav}>안녕하세요</h1>
+        </div>
+    )
+
+
+}
+
+export default Sidenav;
