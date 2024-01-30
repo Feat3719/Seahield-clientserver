4673f01e98a2fe8459ceeb67f068e2f8de746810
jam2208 <junwoo.park82@icloud.com>
2024-01-30 17:58:40 +0900

    1. 게시판 페이지 생성 2. 페이지네이션 라이브러리 설치

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
-import React from 'react';
-import './App.css';
-import Homepage from './Homepage';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Sidenav from './sidenav/Sidenav';
-
+import React from "react";
+import "./App.css";
+import Homepage from "./Homepage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Sidenav from "./sidenav/Sidenav";
+import Board from "./board/Board";
 
 function App() {
+    const main = <Sidenav />;
+    return (
+        <div>
+            <Sidenav />
 
-  const main = <Sidenav/>;
-  return (
-    <div>
-      <Sidenav/>
-
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Homepage />} />
-          
-        </Routes>
-
-      </BrowserRouter>
-    </div>
-
-    
-  );
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Homepage />} />
+                    <Route path="/board" element={<Board />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/board/Board.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import style from "./Board.module.css";
+
+function Board() {
+    return (
+        <div>
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>No.</th>
+                            <th>Title</th>
+                            <th>Writer</th>
+                            <th>Date</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>제목 예시</td>
+                            <td>작성자 예시</td>
+                            <td>2024-01-30</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div>
+                <div>pagination</div>
+            </div>
+        </div>
+    );
+}
+
+export default Board;
