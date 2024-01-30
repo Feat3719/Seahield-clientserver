0532caedd38912953652bc0eb19fdd9d33153cfd
jam2208 <junwoo.park82@icloud.com>
2024-01-30 18:20:22 +0900

    누락 파일 커밋

--- src/board/Board.jsx
+++ src/board/Board.jsx
@@ -3,29 +3,28 @@ import style from "./Board.module.css";
 
 function Board() {
     return (
-        <div>
-            <div>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>No.</th>
-                            <th>Title</th>
-                            <th>Writer</th>
-                            <th>Date</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>제목 예시</td>
-                            <td>작성자 예시</td>
-                            <td>2024-01-30</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-            <div>
-                <div>pagination</div>
+        <div className="wrapper">
+            <div className="list-wrapper">
+                <div className="list">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>No.</th>
+                                <th>Title</th>
+                                <th>Writer</th>
+                                <th>Date</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>제목 예시</td>
+                                <td>작성자 예시</td>
+                                <td>2024-01-30</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     );
