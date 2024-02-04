5251dec398a623daec26ad7da165e6fbebb468d1
jam2208 <junwoo.park82@icloud.com>
2024-02-04 21:29:37 +0900

    1. 게시판상세 구현(댓글 미포함) 2. 게시판수정 구현

--- src/App.js
+++ src/App.js
@@ -1,44 +1,38 @@
-import './App.css';
-import Homepage from './home/Homepage';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import SignupVer from './signupVer/SignupVer';
-import Signup from './signup/Signup';
-import Signin from './signin/Signin';
-import axios from 'axios';
-import IdFind from './find/IdFind';
+import "./App.css";
+import Homepage from "./home/Homepage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignupVer from "./signupVer/SignupVer";
+import Signup from "./signup/Signup";
+import Signin from "./signin/Signin";
+import axios from "axios";
+import IdFind from "./find/IdFind";
 // import Sidenav from './sidenav/Sidenav';
 import BoardList from "./board/BoardList";
 import BoardDetail from "./board/BoardDetail";
 import BoardUpdate from "./board/BoardUpdate";
 import BoardWrite from "./board/BoardWrite";
 
-axios.defaults.baseURL = "http://devfeat.com"
+axios.defaults.baseURL = "http://devfeat.com";
 
 function App() {
+    return (
+        <>
+            {/* <Sidenav/> */}
 
-  return (
-    <>
-      {/* <Sidenav/> */}
-
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Homepage />} />
-          <Route path='/signin' element={<Signin />} />
-          <Route path='/signupver' element={<SignupVer />} />
-          <Route path='/signup' element={<Signup />} />
-          <Route path='/idfind' element={<IdFind />} />
-          <Route path="/boardlist" element={<BoardList />} />
-                    <Route path="/boarddetail" element={<BoardDetail />} />
-                    <Route path="/boardupdate" element={<BoardUpdate />} />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Homepage />} />
+                    <Route path="/signin" element={<Signin />} />
+                    <Route path="/signupver" element={<SignupVer />} />
+                    <Route path="/signup" element={<Signup />} />
+                    <Route path="/idfind" element={<IdFind />} />
+                    <Route path="/boardlist" element={<BoardList />} />
+                    <Route path="/boarddetail/:id" element={<BoardDetail />} />
+                    <Route path="/boardupdate/:id" element={<BoardUpdate />} />
                     <Route path="/boardwrite" element={<BoardWrite />} />
-        </Routes>
-
-      </BrowserRouter>
-
-
-
-    </>
-
+                </Routes>
+            </BrowserRouter>
+        </>
     );
 }
 
--- src/board/BoardDetail.jsx
+++ src/board/BoardDetail.jsx
@@ -2,12 +2,16 @@ import React, { useEffect, useState } from "react";
 import style from "./BoardDetail.module.css";
 import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
+// import { useLocation } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 function BoardDetail() {
     const { id } = useParams();
     const [post, setPost] = useState(null);
+    // const location = useLocation();
+    // const initialPost = location.state ? location.state.post : {};
+    // const [post, setPost] = useState(initialPost);
 
     useEffect(() => {
         const fetchPost = async () => {
@@ -25,8 +29,7 @@ function BoardDetail() {
 
     const handleDelete = async () => {
         try {
-            const response = await axios.delete(`/api/board/article/${id}`);
-            console.log(response.data);
+            await axios.delete(`/api/board/article/${id}`);
             navigate("/boardlist");
         } catch (error) {
             console.error("Error", error);
@@ -34,62 +37,75 @@ function BoardDetail() {
     };
 
     return (
-        <div id={style.container}>
-            <div id={style.detail_box}>
-                <table className={style.table}>
-                    <thead>
-                        <tr>
-                            <th className={style.number}>{post.qnaBoardId}</th>
-                            <th colSpan={5} className={style.title}>
-                                {post.qnaBoardTitle}
-                            </th>
-                            <th className={style.writer}>
-                                {post.qnaBoardWriter}
-                            </th>
-                        </tr>
-                        <tr>
-                            <th className={style.category}>분류</th>
-                            <td colSpan={3} className={style.category_blank}>
-                                {post.qnaBoardCtgr}
-                            </td>
-                            <th colSpan={2} className={style.reads}>
-                                조회수
-                            </th>
-                            <td className={style.reads_blank}>
-                                {post.qnaBoardViewCounts}
-                            </td>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td className={style.content} colSpan={7}>
-                                {post.qnaBoardContents}
-                            </td>
-                        </tr>
-                    </tbody>
-                    <tfoot>
-                        <tr>
-                            <th>작성일</th>
-                            <td colSpan={3}>{post.qnaBoardCreatedDate}</td>
-                            <th colSpan={2}>수정일</th>
-                            <td>{post.qnaBoardUpdateDate}</td>
-                        </tr>
-                    </tfoot>
-                </table>
-            </div>
-            <div id={style.button_box}>
-                <Link to="/boardupdate">
-                    <button className={style.update_button}>수정</button>
-                </Link>
+        post && (
+            <div id={style.container}>
+                <div id={style.detail_box}>
+                    <table className={style.table}>
+                        <thead>
+                            <tr>
+                                <th className={style.number}>
+                                    {post.qnaArticleId}
+                                </th>
+                                <th colSpan={5} className={style.title}>
+                                    {post.qnaArticleTitle}
+                                </th>
+                                <th className={style.writer}>{post.userId}</th>
+                            </tr>
+                            <tr>
+                                <th className={style.category}>분류</th>
+                                <td
+                                    colSpan={3}
+                                    className={style.category_blank}
+                                >
+                                    {post.qnaArticleCtgr}
+                                </td>
+                                <th colSpan={2} className={style.reads}>
+                                    조회수
+                                </th>
+                                <td className={style.reads_blank}>
+                                    {post.qnaArticleViewCounts}
+                                </td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td className={style.content} colSpan={7}>
+                                    {post.qnaArticleContents}
+                                </td>
+                            </tr>
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <th>작성일</th>
+                                <td colSpan={3}>
+                                    {post.qnaArticleCreatedDate}
+                                </td>
+                                <th colSpan={2}>수정일</th>
+                                <td>{post.qnaArticleUpdateDate}</td>
+                            </tr>
+                        </tfoot>
+                    </table>
+                </div>
+                <div id={style.button_box}>
+                    <Link to="/boardlist">
+                        <button className={style.list_button}>목록</button>
+                    </Link>
+                    <Link to={`/boardupdate/${id}`}>
+                        <button className={style.update_button}>수정</button>
+                    </Link>
 
-                <button className={style.delete_button} onClick={handleDelete}>
-                    삭제
-                </button>
+                    <button
+                        className={style.delete_button}
+                        onClick={handleDelete}
+                    >
+                        삭제
+                    </button>
 
-                {/* <button className={style.comment_button}>댓글달기</button> */}
+                    {/* <button className={style.comment_button}>댓글달기</button> */}
+                </div>
+                <div id={style.comment_box}>댓글</div>
             </div>
-            <div id={style.comment_box}>댓글</div>
-        </div>
+        )
     );
 }
 
