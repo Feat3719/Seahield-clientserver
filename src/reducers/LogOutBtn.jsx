d60b4879f1c8fd06c98b2ff96f66eba4dff55bf0
ITYRANNOSAURUS <po04134@naver.com>
2024-02-15 14:47:44 +0900

    FIX : LogOutBtn, Signin 수정

--- src/reducers/LogOutBtn.jsx
+++ src/reducers/LogOutBtn.jsx
@@ -1,38 +1,41 @@
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import style from "./LogOutBtn.module.css";
+import style from "../sidenav/Sidenav.module.css";
+import Swal from "sweetalert2";
 
 function LogOutBtn() {
     const dispatch = useDispatch();
-    // Redux 상태에서 userId 가져오기
-    const userId = useSelector((state) => state.auth.user);
+    const accessToken = useSelector((state) => state.auth.accessToken);
     const navigate = useNavigate();
 
     const handleLogout = async () => {
-        if (userId) {
-            // 서버에 로그아웃 요청 보내기
-            try {
+        try {
+            if (accessToken.length > 0) {
                 await axios.post("/api/auth/signout", {}, { withCredentials: true });
-                // 쿠키에서 refreshToken 삭제
-                document.cookie =
-                    "refreshToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-            } catch (error) {
-                // alert("다시 시도해주시기 바랍니다.");
+                dispatch({ type: "LOGOUT" });
+                navigate("/");
+            } else {
+                Swal.fire({
+                    icon: "error",
+                    title: "잘못된 접근입니다.",
+                });
+                dispatch({ type: "LOGOUT" });
+                navigate("/");
             }
+        } catch (error) {
+            Swal.fire({
+                icon: "error",
+                title: "서버 오류",
+                text: "다시 시도해 주시기 바랍니다.",
+            });
         }
-
-        // Redux 상태 업데이트
-        dispatch({ type: "LOGOUT" });
-        navigate('/');
-        // 홈페이지로 리디렉션
-        // window.location.href = "/";
     };
 
     return (
-        <p className={style.logout} onClick={handleLogout}>
+        <span className={style.itemTxt} onClick={handleLogout}>
             로그아웃
-        </p>
+        </span>
     );
 }
 
