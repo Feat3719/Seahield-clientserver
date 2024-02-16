48cd24c04a5e0aa821fa9e4e1afa416f86acfb2e
quack <rudals23yoo@naver.com>
2024-02-16 12:48:38 +0900

    fix : pagination 수정

--- src/board/Pagination.jsx
+++ src/board/Pagination.jsx
@@ -7,45 +7,50 @@ const Pagination = ({ postsPerPage, totalPosts, paginate, currentPage }) => {
         pageNumbers.push(i);
     }
 
-    // 최대 표시할 페이지 번호 수
-    const maxPageNumberLimit = 10;
-    const minPageNumberLimit = 0;
+    const currentPageGroup = Math.floor((currentPage - 1) / 10);
+    const startPage = currentPageGroup * 10 + 1;
+    const endPage = Math.min((currentPageGroup + 1) * 10, pageNumbers.length);
+
+    const currentPages = pageNumbers.slice(startPage - 1, endPage);
 
     return (
         <div id={style.pagination_box}>
             <nav className={style.nav}>
-                <ul className={style.page_ul}>
-                    <li className={style.page_li} onClick={() => paginate(1)}>
-                        {"<<"}
-                    </li>
-                    <li
-                        className={style.page_li}
-                        onClick={() => paginate(currentPage > 1 ? currentPage - 1 : 1)}
-                    >
-                        {"<"}
-                    </li>
-                    {pageNumbers.map((number) => (
-                        number <= maxPageNumberLimit + minPageNumberLimit &&
-                        number > minPageNumberLimit && (
-                            <li
-                                key={number}
+                <div className={style.page_ul}>
+                    {startPage > 1 && (
+                        <div
+                            className={style.page_li}
+                            onClick={() => paginate(startPage - 1)}
+                        >
+                            <div className={style.page_span}>Pre</div>
+                        </div>
+                    )}
+                    {currentPages.map((number) => (
+                        <div
+                            key={number}
+                            className={`${style.page_li} ${
+                                number === currentPage
+                                    ? style.currentPage
+                                    : style.otherPage
+                            }`}
+                        >
+                            <div
                                 onClick={() => paginate(number)}
-                                className={`${style.page_li} ${currentPage === number ? style.active : ''}`} // 현재 페이지일 경우 active 클래스 적용
+                                className={style.page_span}
                             >
                                 {number}
-                            </li>
-                        )
+                            </div>
+                        </div>
                     ))}
-                    <li
-                        className={style.page_li}
-                        onClick={() => paginate(currentPage < pageNumbers.length ? currentPage + 1 : pageNumbers.length)}
-                    >
-                        {">"}
-                    </li>
-                    <li className={style.page_li} onClick={() => paginate(pageNumbers.length)}>
-                        {">>"}
-                    </li>
-                </ul>
+                    {endPage < pageNumbers.length && (
+                        <div
+                            className={style.page_li}
+                            onClick={() => paginate(endPage + 1)}
+                        >
+                            <div className={style.page_span}>Next</div>
+                        </div>
+                    )}
+                </div>
             </nav>
         </div>
     );
