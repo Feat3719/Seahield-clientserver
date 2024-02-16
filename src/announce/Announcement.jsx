91207b0c9be97c50a08782ce2df18f60988af4f3
jam2208 <junwoo.park82@icloud.com>
2024-02-16 14:39:46 +0900

    1. 공고 게시판 CSS 통일화 진행

--- src/announce/Announcement.jsx
+++ src/announce/Announcement.jsx
@@ -3,27 +3,30 @@ import style from "./Announcement.module.css";
 import AnnounceList from "./AnnounceList";
 import Sidenav from "../sidenav/Sidenav";
 import { useSelector } from "react-redux";
+import Wrapper from "../pagechange/Wrapper";
 
 function Announcement() {
     const userType = useSelector((state) => state.auth.userType);
 
     return (
-        <div id={style.announceTabContainer}>
-            <div className={style.login_nav}>
-                <Sidenav />
-            </div>
-            <div id={style.pageTitleBox}>
-                <div className={style.pageTitle}>공고 게시판</div>
-            </div>
+        <Wrapper>
+            <div id={style.announceTabContainer}>
+                <div className={style.login_nav}>
+                    <Sidenav />
+                </div>
+                <div id={style.pageTitleBox}>
+                    <div className={style.pageTitle}>공고 게시판</div>
+                </div>
 
-            <div id={style.listBox}>
-                <AnnounceList
-                    category={"ANNOUNCE"}
-                    tabName={"공고"}
-                    userType={userType}
-                />
+                <div id={style.listBox}>
+                    <AnnounceList
+                        category={"ANNOUNCE"}
+                        tabName={"공고"}
+                        userType={userType}
+                    />
+                </div>
             </div>
-        </div>
+        </Wrapper>
     );
 }
 
